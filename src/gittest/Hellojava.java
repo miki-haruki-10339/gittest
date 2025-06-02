12ad6777a8f356e744f17178e430b5e018ad95ca
三木春輝 <mikih@revolvesys.co.jp>
2025-06-02 16:59:53 +0900

    似知川更新

--- src/gittest/Hellojava.java
+++ src/gittest/Hellojava.java
@@ -4,5 +4,7 @@ public class Hellojava {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("Hello World");
+		System.out.println("似知川です");
+		
 	}
 }
